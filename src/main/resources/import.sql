31820f72a375a098bcb4d2872b8c46a16dfc059f
Your Name <you@example.com>
2019-06-19 16:57:32 +0100

    renamed html elements for clarity

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,13 +4,16 @@ INSERT INTO KAIJUACCOUNT (name, height, weight, creatureType, description) VALUE
 INSERT INTO KAIJUACCOUNT (name, height, weight, creatureType, description) VALUES ('Godzilla', '100','300', 'Lizard', 'The original king of kaiju')
 
 
-INSERT INTO KAIJUACCOUNT (name, height, weight, creatureType, description) VALUES ('Mothra', '100','150', 'Moth', 'The benevolent Moth goddess')
+INSERT INTO KAIJUACCOUNT (name, height, weight, creatureType, description) VALUES ('Mothra', '100','129', 'Moth', 'The benevolent Moth goddess')
 
 
-INSERT INTO KAIJUACCOUNT (name, height, weight, creatureType, description) VALUES ('King Ghidorrah', '100','300', 'Alien Demon', 'The three headed, bat winged alien invader and rival to GodZilla')
+INSERT INTO KAIJUACCOUNT (name, height, weight, creatureType, description) VALUES ('King Ghidorrah', '100','356', 'Alien Demon', 'The three headed, bat winged alien invader and rival to GodZilla')
 
 
-INSERT INTO KAIJUACCOUNT (name, height, weight, creatureType, description) VALUES ('Mecha Godzilla', '100','300', 'Robot Lizard', 'A mechanized giant made from the removed head of King Ghidorrah')
+INSERT INTO KAIJUACCOUNT (name, height, weight, creatureType, description) VALUES ('Mecha Godzilla', '100','347', 'Robot Lizard', 'A mechanized giant made from the removed head of King Ghidorrah')
+
+
+INSERT INTO KAIJUACCOUNT (name, height, weight, creatureType, description) VALUES ('Rodan', '150','300', 'Flaming Pterodactyl', 'The lava born dragon bird')
 
 
 INSERT INTO FILMDATA ( name, noOfFilms, firstFilm, winCount) VALUES ('Zilla', '2', 'Godzilla 1998', '0')
@@ -19,7 +22,9 @@ INSERT INTO FILMDATA ( name, noOfFilms, firstFilm, winCount) VALUES ('Godzilla',
 
 INSERT INTO FILMDATA ( name, noOfFilms, firstFilm, winCount) VALUES ('Mothra', '16' , 'Mothra 1961', '14')
 
-INSERT INTO FILMDATA ( name, noOfFilms, firstFilm, winCount) VALUES ('King Ghidorrah', '9', ' Ghidorah, the Three-Headed Monster 1974', '3')
+INSERT INTO FILMDATA ( name, noOfFilms, firstFilm, winCount) VALUES ('King Ghidorrah', '9', ' Ghidorah, the Three-Headed Monster 1964', '3')
 
-INSERT INTO FILMDATA ( name, noOfFilms, firstFilm, winCount) VALUES ('Mecha Godzilla', '16' , 'Godzilla vs. Mechagodzilla 1961', '2')
+INSERT INTO FILMDATA ( name, noOfFilms, firstFilm, winCount) VALUES ('Mecha Godzilla', '16' , 'Godzilla vs. Mechagodzilla 1974', '2')
+
+INSERT INTO FILMDATA ( name, noOfFilms, firstFilm, winCount) VALUES ('Rodan', '12' , 'Rodan 1956', '4')
 
