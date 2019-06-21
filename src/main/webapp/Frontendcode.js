28f902c52f211dd95bdafaa3ed158e68615820e6
Your Name <you@example.com>
2019-06-21 10:40:31 +0100

    Restyled for user experience and

--- src/main/webapp/Frontendcode.js
+++ src/main/webapp/Frontendcode.js
@@ -36,8 +36,7 @@ const clickActions = {
     createKaijuAcc: () => buttonClick('POST', 'http://35.228.254.158:8888/SoloProject/api/kaijuAccount/createKaijuAccount', createkaijuAccount()),
 
     updateKaijuAcc: () => buttonClick("PUT", 'http://35.228.254.158:8888/SoloProject/api/kaijuAccount/updateKaijuAccount/' + getaccname(), updateAccount()),
-    //getFilmbyname: () => buttonClick('GET', 'http://localhost:8080/SoloProject/api/FilmData/getAFilmData/' + getaccname()),
-   // updateKaijuAcc: () => buttonClick("PUT", 'http://35.246.172.168:8888/SoloProject/api/kaijuAccount/updateKaijuAccount/' + getaccname(), updateAccount()),
+
 
 };
 function buttonClick(reqType, url, body) {
@@ -90,23 +89,18 @@ function resolved(result) {
 
             tr += "<td>" + result[c].name + "</td> </tr>";
 
-            // let node = document.createElement("div");
-            // node.setAttribute("id", "resInner");
-            // document.getElementById("results").appendChild(node);
-            // node.appendChild(textnode);
+
         }
         tbody.innerHTML += tr;
     }
     else {
         tr += "<td> ___Codename___ </td> <td> ___Height/Wingspan(feet)___ </td> <td> ___Weight(lbs)___ </td> <td> ___genus___ </td> <td> ___Description___ </td> <td> ___more___ </td></tr>";
 
-        let btn = '<input class="btn btn-success btn btn-primary btn mx-auto font-weight-bold" id="btnfilm" type="button" name="filmbutton" value="filmbutton" onclick="filmDetailButton()">'
+        let btn = '<input class="btn btn-info btn btn-primary btn mx-auto font-weight-bold" id="btnfilm" type="button" name="filmbutton" value="filmbutton" onclick="filmDetailButton()">'
 
         tr += "<td>" + result.name + "</td> <td>" + result.height + "</td> <td>" + result.weight + "</td> <td>" + result.creatureType + "</td> <td>" + result.description + "</td> <td>" + btn + "</td> </tr>";
-        //  let textnode = document.createTextNode(output);
 
 
-        // node.appendChild(textnode);
         tbody.innerHTML += tr;
 
     }
